f6c9e27eec26ebca003c22ab7db5c57d4dba9330
時流 <jiru@users.noreply.github.com>
2014-10-03 10:35:29 +0000

    Move the UTF-8 function to a separate file.
    So that it can be used by the Kanji class too.

--- src/Kana.cpp
+++ src/Kana.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <cstdint>
 
+#include "Utf8.h"
 #include "Kana.h"
 
 std::unordered_map<uint32_t, uint32_t> Kana::KataToHiraTable;
@@ -38,36 +39,6 @@ Kana::Kana() {
         uint32_t hiragana = *(uint32_t*)Kana::hiraganas[i];
         Kana::KataToHiraTable.insert(std::make_pair(katakana, hiragana));
     }
-}
-
-/* Very naive UTF-8 parsing implementation. */
-static int utf8_getc(const char **str, char *character, int characterLen) {
-    /* mask values for bit pattern of first byte in multi-byte
-     * UTF-8 sequences: 
-     *   192 - 110xxxxx - for U+0080 to U+07FF 
-     *   224 - 1110xxxx - for U+0800 to U+FFFF 
-     *   240 - 11110xxx - for U+010000 to U+1FFFFF
-     */
-    const unsigned short mask[] = {255, 192, 224, 240};
-    int i;
-
-    for (i = 0; i < characterLen; i++) {
-        character[i] = '\0';
-    }
-
-    character[0] = **str;
-    for (
-         i = 1;
-         i < 4 && i < characterLen && **str != '\0'
-              && (character[0] & mask[i]) == mask[i];
-         i++
-    ) {
-        (*str)++;
-        character[i] = **str;
-    }
-    (*str)++;
-
-    return (character[0] != '\0');
 }
 
 std::string Kana::katakana_to_hiragana(std::string katakana) {
---
+++ src/Utf8.h
@@ -0,0 +1,6 @@
+#ifndef NIHONGOPARSER_UTF8_H
+#define NIHONGOPARSER_UTF8_H
+
+int utf8_getc(const char **str, char *character, int characterLen);
+
+#endif
